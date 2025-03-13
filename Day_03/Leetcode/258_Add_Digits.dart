3e2c6099dc04295273eb28bbd936be10d44f82ca
RH_Konok <104697394+Rakibul-Hasan-Konok@users.noreply.github.com>
2025-03-13 23:28:20 +0600

    Done 13-03-25 with leetcode Easy roblem

---
+++ Day_03/Leetcode/258_Add_Digits.dart
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/add-digits/
+//Easy
+
+class Solution {
+  int addDigits(int num) {
+  while (num >= 10) {
+    int sum = 0;
+    while (num > 0) {
+      sum += num % 10;
+      num ~/= 10;
+    }
+    num = sum;
+  }
+  return num;
+}
+}
